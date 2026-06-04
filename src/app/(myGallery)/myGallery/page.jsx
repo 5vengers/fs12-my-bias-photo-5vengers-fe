4f7312ad7639acbac50cde7b9f3ccbe65570c499
9g-g9 <nayun12375@gmail.com>
2026-06-04 11:35:42 +0900

    feat: 기초 작업

---
+++ src/app/(myGallery)/myGallery/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const MyGallery = () => {
+  return (
+    <div>
+      <div>헤더</div>
+
+      <div>페이지 헤더</div>
+
+      <div>
+        <div>
+          <p>유디님이 보유한 포토카드</p>
+          <p>(40장)</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default MyGallery;
