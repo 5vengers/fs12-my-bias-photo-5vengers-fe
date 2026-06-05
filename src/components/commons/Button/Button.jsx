4fed627e08891851dfa41cb24040b99ef867d7fb
9g-g9 <nayun12375@gmail.com>
2026-06-05 11:41:42 +0900

    style: 버튼 주석 및 기본값  추가

--- src/components/commons/Button/Button.jsx
+++ src/components/commons/Button/Button.jsx
@@ -1,17 +1,21 @@
 const Button = ({
   children,
   size = 'lg',
-  isThick,
+  isThick = true,
   active,
   btnType = 'button',
-  type,
+  type = 'pri',
 }) => {
+  // true 면 thick, false 면 thin
   const thickness = ['h-[43px]', 'h-[52px]'];
+
+  // 기본 lg
   const sizing = {
     sm: 'w-[150px]',
     lg: 'w-[520px]',
   };
 
+  // 기본 pri (노란색)
   const typeColor = {
     pri: 'bg-main text-black',
     sec: 'bg-black text-white border border-white',
