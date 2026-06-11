cbf97d543718fbaeb1f187c224a1adf5eef715c3
9g-g9 <nayun12375@gmail.com>
2026-06-11 10:37:17 +0900

    fix: select 제외 onClick 및 onChange 함수 props 처리

--- src/components/commons/Input/Textarea.jsx
+++ src/components/commons/Input/Textarea.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-const Textarea = ({ onChange }) => {
+const Textarea = ({ ...props }) => {
   return (
     <>
       <textarea
@@ -8,7 +8,7 @@ const Textarea = ({ onChange }) => {
         id="textarea-input"
         className="h-[180px] w-full border border-white px-[20px] py-[12px] focus:bg-gray-500"
         placeholder="포토카드 설명을 입력해주세요."
-        onChange={(e) => onChange(e)}
+        {...props}
       />
     </>
   );
