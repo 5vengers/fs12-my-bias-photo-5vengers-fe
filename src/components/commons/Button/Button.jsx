9f3f26aab818830778bf3c5de66d1fb9e260fb32
9g-g9 <nayun12375@gmail.com>
2026-06-15 17:36:49 +0900

    style: button 주석 수정

--- src/components/commons/Button/Button.jsx
+++ src/components/commons/Button/Button.jsx
@@ -1,6 +1,6 @@
 /*
   children = 버튼에 들어갈 text 및 html
-  size = 버튼 사이즈 'lg, 'sm' (기본 'lg')
+  size = 버튼 사이즈 'full', 'lg', 'sm' (기본 'full')
   isThick = 버튼 두껍기 (기본 true)
   disabled = 버튼 활성화 (기본 false)
   btnType = 버튼 타입 'button', 'submit' (기본 'button')
