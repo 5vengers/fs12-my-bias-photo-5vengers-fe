dca7708fe8f71be3165ebad4298d2bbf2715e9d6
9g-g9 <nayun12375@gmail.com>
2026-06-16 09:20:28 +0900

    fix: home 원복

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,3 @@
-'use client';
-
-import Pagination from '@/components/commons/Pagination/Pagination';
-import { useState } from 'react';
-
 export default function Home() {
-  const [page, setPage] = useState(1);
-
-  const onPageChange = (num) => {
-    setPage(num);
-  };
-
-  return (
-    <div>
-      메인 화면
-      <Pagination
-        currentPage={page}
-        totalPages={20}
-        onPageChange={onPageChange}
-      />
-    </div>
-  );
+  return <div>메인 화면</div>;
 }
