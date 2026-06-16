541404442d496c2cc303beee6b5fc2465c06a2a4
Obebe <shkim000905@naver.com>
2026-06-16 13:37:10 +0900

    fix: useMutation, useQueryClient import

--- src/hooks/usePoint.js
+++ src/hooks/usePoint.js
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import pointService from '@/libs/service/pointService';
 import { useIsAuthenticated } from '@/hooks/useAuth';
 
