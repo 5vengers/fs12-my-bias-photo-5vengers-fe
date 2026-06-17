64c70315267b1a7f8be416cb5f911651c0d270ce
Obebe <shkim000905@naver.com>
2026-06-17 11:11:46 +0900

    fix: useMarket import 수정

--- src/hooks/useMarket.js
+++ src/hooks/useMarket.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import marketService from '@/libs/service/marketService';
+import { getMarketItemDetail } from '@/libs/service/marketService';
 
 export const MARKET_QUERY_KEYS = {
   DETAIL: (itemId) => ['marketItems', 'detail', itemId],
@@ -8,7 +8,7 @@ export const MARKET_QUERY_KEYS = {
 export const useMarketItemDetail = (itemId) => {
   return useQuery({
     queryKey: MARKET_QUERY_KEYS.DETAIL(itemId),
-    queryFn: () => marketService.getMarketItemDetail(itemId),
+    queryFn: () => getMarketItemDetail(itemId),
     enabled: !!itemId,
   });
 };
