e5a14d476ccb29e26f095feb23e65a83d6511e12
LEE SANG YUN <sensertive05@gmail.com>
2026-06-17 14:53:21 +0900

    refactor: 폴더 구조 변경 (#59 반영) - (myGallery)/mySales -> (private)/(myGallery)/mySales 이동 - (privatePage), (publicPage) 폴더 제거

--- src/app/(privatePage)/layout.jsx
+++
@@ -1,7 +0,0 @@
-import { PrivateGuard } from '@/components/commons/AuthGuard/AuthGuard';
-
-const PrivateLayout = ({ children }) => {
-  return <PrivateGuard>{children}</PrivateGuard>;
-};
-
-export default PrivateLayout;
