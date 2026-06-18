17da844c6571474e598cdbf3595a721a366f26cc
sebikawa32 <tpqlsdl150@naver.com>
2026-06-18 18:17:37 +0900

    chore: 카드 로고 중복 코드 삭제

--- src/components/MarketPage/MarketCard.jsx
+++ src/components/MarketPage/MarketCard.jsx
@@ -23,9 +23,6 @@ function MarketCard({ item }) {
           />
           <Card.SaleInfo title={'수량'} count={item.quantity} />
         </Card.SaleInfoLayout>
-        <div className="mt-[20px] flex items-center justify-center pt-[15px]">
-          <Image src={logoImage} alt="브랜드 로고" width={99} height={18} />
-        </div>
       </Card>
     </Link>
   );
