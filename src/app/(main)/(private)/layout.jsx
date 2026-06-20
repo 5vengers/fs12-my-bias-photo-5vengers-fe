92123c4da34c3763e0860a987b9430152f257ae4
jeongmin00 <milk001123@naver.com>
2026-06-21 01:07:25 +0900

    feat: private layout에 SSEProvider 추가

--- src/app/(main)/(private)/layout.jsx
+++ src/app/(main)/(private)/layout.jsx
@@ -1,7 +1,12 @@
 import { PrivateGuard } from '@/components/commons/AuthGuard/AuthGuard';
+import SSEProvider from '@/components/commons/Notification/SSEProvider';
 
 const PrivateLayout = ({ children }) => {
-  return <PrivateGuard>{children}</PrivateGuard>;
+  return (
+    <PrivateGuard>
+      <SSEProvider>{children}</SSEProvider>
+    </PrivateGuard>
+  );
 };
 
 export default PrivateLayout;
