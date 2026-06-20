e8db05cee36f635dd7e479582debc837d5cb301a
jeongmin00 <milk001123@naver.com>
2026-06-20 14:56:52 +0900

    fix: loggedInToastVariant 잘못된 import로 신규가입 토스트 미노출 버그 수정

--- src/components/commons/Toast/LoggedInToast.jsx
+++ src/components/commons/Toast/LoggedInToast.jsx
@@ -3,13 +3,15 @@
 import { useEffect, useState } from 'react';
 import Toast from '@/components/commons/Toast/Toast';
 import useAuthStore from '@/store/authStore';
-import loggedInToastVariant from '@/store/authStore';
 
 const LoggedInToast = () => {
   const [showToast, setShowToast] = useState(false);
   const [toastVariant, setToastVariant] = useState('login');
 
   const showLoggedInToast = useAuthStore((state) => state.showLoggedInToast);
+  const loggedInToastVariant = useAuthStore(
+    (state) => state.loggedInToastVariant,
+  );
   const setShowLoggedInToast = useAuthStore(
     (state) => state.setShowLoggedInToast,
   );
