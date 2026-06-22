4d68efc8288ce05669fca4d669e8ae11baf0bfb9
sebikawa32 <tpqlsdl150@naver.com>
2026-06-23 00:13:16 +0900

    fix: 공통 모달 overlayClass 추가

--- src/components/commons/Modal/ModalMain.jsx
+++ src/components/commons/Modal/ModalMain.jsx
@@ -1,6 +1,8 @@
-const ModalMain = ({ children }, className = '') => {
+const ModalMain = ({ children, className = '', overlayClassName = '' }) => {
   return (
-    <div className="fixed z-[9999] flex h-full w-full items-center justify-center bg-black/50">
+    <div
+      className={`fixed z-[9999] flex h-full w-full items-center justify-center bg-black/50 ${overlayClassName}`}
+    >
       <div
         className={`relative flex flex-col items-center gap-[40px] rounded-xs bg-gray-500 p-[80px] ${className || ''}`}
       >
