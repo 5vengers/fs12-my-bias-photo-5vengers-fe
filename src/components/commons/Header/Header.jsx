a91a648fb6d62bc242d920e8d57dc28e2a3a81d1
9g-g9 <nayun12375@gmail.com>
2026-06-22 17:13:35 +0900

    design: 반응형 헤더 추가, fix: useMouseOut 기능 수정

--- src/components/commons/Header/Header.jsx
+++ src/components/commons/Header/Header.jsx
@@ -3,7 +3,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Logo from '@/assets/images/img-logo.svg';
-import PointIcon from '@/assets/icons/ico-point.svg';
+import PointIcon from '@/assets/icons/ic-point.svg';
+import MenuIcon from '@/assets/icons/ic-menu.svg';
 import styles from './Header.module.css';
 import useAuthStore from '@/store/authStore';
 import { useIsAuthenticated, useLogout } from '@/hooks/useAuth';
@@ -12,16 +13,20 @@ import Profile from './Profile';
 import { useState } from 'react';
 import { useSurpriseModalStore } from '@/store/supriseStore';
 import NotificationBell from '@/components/commons/Notification/NotificationBell';
+import useMouseOut from '@/hooks/useMouseOut';
 
 const Header = () => {
   const user = useAuthStore((state) => state.user);
 
   const [isOpen, setIsOpen] = useState(false);
+  const [isMobileOpen, setIsMobileOpen] = useState(false);
 
   const isLogin = useIsAuthenticated();
   const { mutate: logout } = useLogout();
 
   const { data: point, isPending, isError } = useMyPoint();
+  const { ref } = useMouseOut({ setIsOpen: setIsOpen });
+  const { ref: mobileRef } = useMouseOut({ setIsOpen: setIsMobileOpen });
 
   // 깜짝 모달 관리
   // open -> 모달 open
@@ -41,7 +46,14 @@ const Header = () => {
   })();
 
   return (
-    <div className="flex items-center justify-between px-[220px] py-[27px]">
+    <div className="tablet:px-10 tablet:py-6.5 mobile:p-5 flex items-center justify-between px-55 py-7">
+      {/* 모바일 메뉴 */}
+      <div className="mobile:block hidden">
+        <button type="button" onClick={() => setIsMobileOpen(true)}>
+          <Image src={MenuIcon} alt="햄버거 메뉴" width={24} height={24} />
+        </button>
+      </div>
+
       <div className="cursor-pointer">
         <Link href={isLogin ? '/market' : '/'}>
           <Image
@@ -54,7 +66,7 @@ const Header = () => {
         </Link>
       </div>
 
-      <ul className={`flex items-center justify-center gap-[30px] text-sm`}>
+      <ul className="mobile:hidden flex items-center justify-center gap-[30px] text-sm">
         {isLogin ? (
           <>
             <li className="font-bold text-gray-200">{pointText}</li>
@@ -77,6 +89,7 @@ const Header = () => {
               </button>
               {isOpen && (
                 <Profile
+                  ref={ref}
                   username={user?.nickname || '회원'}
                   point={pointText}
                   setIsOpen={setIsOpen}
@@ -105,6 +118,63 @@ const Header = () => {
           </>
         )}
       </ul>
+
+      {/* 모바일 */}
+      <div className="mobile:block hidden">
+        {isLogin ? <NotificationBell /> : <Link href={'/login'}>로그인</Link>}
+      </div>
+
+      <div className="mobile:flex absolute fixed top-0 left-0 z-60 hidden">
+        {isMobileOpen && (
+          <>
+            <div className="z-5 h-screen w-screen bg-black/50"></div>
+            {isLogin ? (
+              <Profile
+                ref={mobileRef}
+                username={user?.nickname || '회원'}
+                point={pointText}
+                setIsOpen={setIsMobileOpen}
+              >
+                <button
+                  type="button"
+                  aria-label="포인트 팝업"
+                  onClick={() => {
+                    open();
+                    setIsMobileOpen(false);
+                  }}
+                  className={`relative flex w-full items-center justify-between text-sm font-bold${canGetPoint ? pointbedge : ''}`}
+                >
+                  포인트 박스 확인{' '}
+                  <Image src={PointIcon} width={20} height={20} alt="포인트" />
+                </button>
+                <Link href="/logout" className="text-sm text-gray-500">
+                  로그아웃
+                </Link>
+              </Profile>
+            ) : (
+              <div
+                ref={mobileRef}
+                className="absolute top-0 left-0 z-4 h-screen w-65 bg-gray-500"
+              >
+                <div className="flex flex-col items-start gap-5 border-b border-gray-300 p-5">
+                  <Image
+                    src={Logo}
+                    alt={'모바일 로고'}
+                    width={140}
+                    height={25}
+                    priority
+                  />
+                </div>
+                <nav className="flex flex-col items-start gap-4 p-5">
+                  <Link href="/register" className="text-sm font-bold">
+                    회원가입
+                  </Link>
+                </nav>
+              </div>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
